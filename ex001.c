55538e50ac293313409388672577b609dd7c9521
Paulo Medrado da Silva Junior <junio_medrado@hotmail.com>
2025-06-23 14:54:18 -0300

    Add files via upload

---
+++ ex001.c
@@ -0,0 +1,21 @@
+#include <stdio.h>
+
+int main(){
+    int idade;
+    //int idade = 25;
+    //float altura = 1.75;
+    // letra = 'a';
+    //char nome[20] = "Paulo Silva";
+
+    //printf("Idade: %d\n", idade);
+    //printf("Altura: %.2f\n", altura);
+    //printf("letra: %c\n", letra);
+    //printf("Nome: %s\n", nome);
+    //printf("muito bom !\n");
+    printf("Digite sua idade:");
+    scanf("%d", &idade);
+    printf("idade: %d\n", idade);
+    //scanf("Digite sua altura; %f\n", &altura);
+    //printf("altura: %.2f\n", altura);
+    return 0;
+}
